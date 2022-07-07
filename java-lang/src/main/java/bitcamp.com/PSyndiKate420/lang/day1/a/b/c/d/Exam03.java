940d9303ee212a0f03910e2ec15e5b48fc2b4b80
PSyndiKate420 <SyndiKate420@gmail.com>
2022-07-07 10:01:00 +0900

    백업

---
+++ java-lang/src/main/java/bitcamp.com/PSyndiKate420/lang/day1/a/b/c/d/Exam03.java
@@ -0,0 +1,14 @@
+package a.b.c.d;
+
+public class Exam03 {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+  void m2() {
+    System.out.println("Hello!");
+  }
+  // void m2()를 실행시키고 싶다면, main method에서 void m2()를 호출해야 함.
+}
+
+// class가 실행되려면 main method가 존재해야 한다.
+// ex) public static void main(String[] args)
