b20b932e788b92d072d778dcc0b1bb1a82fbabba
PSyndiKate420 <SyndiKate420@gmail.com>
2022-09-13 20:28:10 +0900

    .

--- board-App/app-server/src/main/java/com/bitcamp/board/ServerApp05.java
+++ board-App/app-server/src/main/java/com/bitcamp/board/ServerApp05.java
@@ -9,7 +9,7 @@ import com.bitcamp.board.servlet.BoardServlet;
 import com.bitcamp.board.servlet.MemberServlet;
 import com.bitcamp.servlet.Servlet;
 
-public class ServerApp05 {
+public class ServerApp04 {
 
   public static void main(String[] args) {
 
@@ -22,6 +22,41 @@ public class ServerApp05 {
     servletMap.put("daily", new BoardServlet("daily"));
     servletMap.put("member", new MemberServlet("member"));
 
+    // 스레드로 만드는 대신에 Thread가 실행할 수 있는 클래스로 변경한다.
+    class RequestRunnable implements Runnable {
+
+      private Socket socket;
+
+      public RequestRunnable(Socket socket) {
+        this.socket = socket;
+      }
+
+      @Override
+      public void run() {
+        try (Socket socket = this.socket;
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+
+          System.out.println("클라이언트와 연결 되었음!");
+
+          String dataName = in.readUTF();
+
+          Servlet servlet = servletMap.get(dataName);
+          if (servlet != null) {
+            servlet.service(in, out);
+          } else {
+            out.writeUTF("fail");
+          }
+
+          System.out.println("클라이언트와 연결을 끊었음!");
+
+        } catch (Exception e) {
+          System.out.println("클라이언트 요청 처리 중 오류 발생!");
+          e.printStackTrace();
+        }
+      }
+    }
+
     System.out.println("[게시글 데이터 관리 서버]");
 
     try (ServerSocket serverSocket = new ServerSocket(8888);) {
@@ -29,33 +64,11 @@ public class ServerApp05 {
       System.out.println("서버 소켓 준비 완료!");
 
       while (true) {
-        new Thread(new Runnable() {
-          Socket socket = serverSocket.accept();
-          @Override
-          public void run() {
-            try (Socket socket = this.socket;
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+        Socket socket = serverSocket.accept();
 
-              System.out.println("클라이언트와 연결 되었음!");
-
-              String dataName = in.readUTF();
-
-              Servlet servlet = servletMap.get(dataName);
-              if (servlet != null) {
-                servlet.service(in, out);
-              } else {
-                out.writeUTF("fail");
-              }
-
-              System.out.println("클라이언트와 연결을 끊었음!");
-
-            } catch (Exception e) {
-              System.out.println("클라이언트 요청 처리 중 오류 발생!");
-              e.printStackTrace();
-            }
-          }
-        }).start();
+        // 클라이언트 요청을 처리할 스레드를 만들고
+        // main 실행 흐름에서 분리하여 별도의 실행 흐름으로 작업을 수행시킨다.
+        new Thread(new RequestRunnable(socket)).start();
       }
     } catch (Exception e) {
       e.printStackTrace();
